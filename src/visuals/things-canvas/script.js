cb520244141126cc039766e648abefd4fa5ed39c
CH <clint@thestaticvoid.net>
2023-09-19 15:12:12 +0200

    Made an even more bare starters/things

--- src/visuals/things-canvas/script.js
+++ src/visuals/things-canvas/script.js
@@ -1,17 +1,21 @@
+import {fullSizeCanvas} from '../../ixfx/dom.js';
+import { Points } from '../../ixfx/geometry.js';
 import * as Things from './thing.js';
 import * as Util from './util.js';
 
 // Settings for sketch
 const settings = Object.freeze({
-  // How often to update the Things
   thingUpdateSpeedMs: 10,
   // How many things to spawn
-  spawnThings: 10
+  spawnThings: 100,
+  hueChange: 0.1
 });
 
 /** 
  * @typedef {{
+ *  hue:number
  *  things:Things.Thing[]
+ *  bounds: import('./util.js').Bounds
  * }} State
  */
 
@@ -19,20 +23,47 @@ const settings = Object.freeze({
  * @type {State}
  */
 let state = Object.freeze({
-  things: []
+  things: [],
+  hue: 0,
+  movement: 0,
+  bounds: {
+    width: 0, height: 0,
+    min:0, max: 0,
+    center: { x: 0, y: 0 },
+  }
 });
 
 /**
  * Makes use of the data contained in `state`
  */
 const use = () => {
-  // 1. Use the state some how
+  const { hue, bounds, things } = state;
+
+  const context = Util.getDrawingContext();
+
+  // 1. Eg. use the ambient state
+  context.fillStyle = `hsl(${hue}, 100%, 90%)`;
+  context.fillRect(0,0,bounds.width,bounds.height);
+  
+  // 2. Use things
+  for (const thing of things) {
+    Things.use(thing, context, bounds);
+  }
 };
 
 const update = () => {
+  const { hueChange } = settings;
+  let { hue } = state;
   // 1. Any other state updates?
+  // eg: cycle hue
+  hue += hueChange;
+
   // 2. Sanity check
+  hue = hue%360; // 0..360 scale
+
   // 3. Save state
+  saveState({ hue });
+
   // 4. Use state
   use();
 
@@ -40,15 +71,32 @@ const update = () => {
   window.requestAnimationFrame(update);
 };
 
-function setup() {
-  // Create things
+function setup () {
+  // Automatically size canvas to viewport
+  fullSizeCanvas(`#canvas`, onResized => {
+    saveState({ bounds: onResized.bounds });
+  });
+    
   const things = [];
   for (let index=1;index<=settings.spawnThings;index++) {
     things.push(Things.create(index));
   }
-
-  // Save them into state
   saveState({ things });
+
+  document.addEventListener(`pointermove`, (event) => {
+    const relativeMovement = Util.addUpMovement(event);
+    const relativePosition = Points.divide({x: event.clientX, y:event.clientY}, state.bounds);
+
+    // Get new thing state
+    let things = state.things.map(
+      thing => Things.onMovement(
+        thing, 
+        relativeMovement,
+        relativePosition)
+    );
+    saveState({ things });
+    
+  });
 
   // Update things at a fixed rate
   setInterval(() => {
@@ -59,11 +107,6 @@ function setup() {
 
     // Save updated things into state
     saveState({ things });
-
-    // Visually update based on new state
-    for (const thing of things) {
-      Things.use(thing);
-    }
   }, settings.thingUpdateSpeedMs);
 
   // Update state of sketch and use state
@@ -97,8 +140,7 @@ function updateThingInState(thingId, updatedThing) {
 
   const things = state.things.map(thing => {
     // Is it the thing we want to change?
-    if (thing.id !== thingId) return thing; // nup
-    
+    if (thing.id !== thingId) return thing; // no
     // Return mutated thing
     completedThing = {
       ...thing,
